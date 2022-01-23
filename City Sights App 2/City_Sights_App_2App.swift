094524185121f2c714555ea9e1bc2aa1fcaf22f6
Angel <angelramirez@artexcomputer.com>
2022-01-23 11:31:07 -0500

    Initial Commit

---
+++ City Sights App 2/City_Sights_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_App_2App.swift
+//  City Sights App 2
+//
+//  Created by ANGEL RAMIREZ on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
